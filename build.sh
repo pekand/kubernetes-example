b586b1496049ae6ead92f9b650c47723644c2952
Andrej Pekar <andrej.pekar@nike.sk>
2022-09-20 02:49:21 +0200

    minikube

--- build.sh
+++ build.sh
@@ -1,28 +1,39 @@
 #!/bin/bash
 
-sudo apt install docker.io
-sudo apt install docker-compose
-sudo systemctl enable docker
-sudo gpasswd -a ${USER} docker
+export http_proxy=http://192.168.168.1:8080 
+export https_proxy=http://192.168.168.1:8080
+export NO_PROXY=localhost,127.0.0.1,10.96.0.0/12,192.168.59.0/24,192.168.49.0/24,192.168.39.0/24
 
+echo "##########################"
 
-sudo snap install microk8s --classic
-sudo usermod -a -G microk8s ${USER}
+minikube delete --all
+minikube start
 
-alias kubectl="microk8s kubectl"
-microk8s enable dns ingress  storage
-microk8s kubectl create configmap nginxconfigmap --from-file=default.conf
-microk8s kubectl apply -f mysql-pv.yaml
-microk8s kubectl apply -f mysql-deployment.yaml
-microk8s kubectl apply -f nginxsecrets.yaml
-microk8s kubectl apply -f deployment.yaml
-microk8s kubectl apply -f Ingress.yaml
+alias kubectl="minikube kubectl --"
+echo "##########################"
+minikube kubectl -- create configmap nginxconfigmap --from-file=default.conf
+echo "##########################"
+minikube kubectl -- apply -f mysql-pv.yaml
+echo "##########################"
+minikube kubectl -- apply -f mysql-deployment.yaml
+echo "##########################"
+minikube kubectl -- apply -f nginxsecrets.yaml
+echo "##########################"
+minikube kubectl -- apply -f deployment.yaml
+echo "##########################"
+minikube kubectl -- apply -f Ingress.yaml
+echo "##########################"
+minikube kubectl -- get nodes
+echo "##########################"
+minikube kubectl -- get pods
+echo "##########################"
+minikube kubectl -- get services
+echo "##########################"
+minikube kubectl -- get ingress
+echo "##########################"
+minikube kubectl -- get events
 
-microk8s kubectl get nodes
-microk8s kubectl get pods
-microk8s kubectl get services
-microk8s kubectl get ingress
+#curl -k https://kubernetes.project.dev
 
-curl -k https://kubernetes.project.dev
-
+#minikube delete --all
 
